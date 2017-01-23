89692329a2c6b4f827cf5605260e3e11ac002ebd
Ross Martin <c13730359@mydit.ie>
2017-01-23 20:23:36 +0000

    commit

--- Man Utd Players.js
+++ Man Utd Players.js
@@ -4,7 +4,7 @@
 	"height": "1.95m",
 	"date of birth": "October 3, 1981",
 	"image":"http://cdn.images.express.co.uk/img/dynamic/67/590x/Zlatan-Ibrahimovic-623921.jpg",
-	"name":"Zlatan Ibrahimovic"
+	"name":"Zlatan Ibrahimovic",
 	"shirt number": 11
 	
 	
@@ -16,7 +16,7 @@
 	"height": "1.76 m",
 	"date of birth": "October 24, 1985 ",
 	"image":"http://www.manutdpics.com/t/2/manchester-united-team-group-photocall-11328653.jpg",
-	"name":"Wayne Rooney"
+	"name":"Wayne Rooney",
 	"shirt number": 9
 	
 	
